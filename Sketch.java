5ea456dcb4b0eeb39ba3b64e31c1f7fc1752a7d5
koolcid Ahmadi <99352795+koolcid13@users.noreply.github.com>
2022-04-11 02:10:53 +0000

    draws a pattern of stick figures and hearts using methods

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,110 @@
 import processing.core.PApplet;
 
+/**
+* A program Sketch.java that uses methods with parameters to draw composite objects at various locations
+* @author: Avin A.
+*
+*/
+
 public class Sketch extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+  // initial settings
+
   public void settings() {
-	// put your size call here
-    size(400, 400);
+    size(800, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
     background(210, 255, 173);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
+  
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    
+    // runs two for's for row and column and draws stick figures each time. Might draw a heart or not.
+	  for (int row = 1; row <= 7; row ++) {
+      for (int col = 1; col <= 15; col ++) {
+        drawGuy ((float) col / 16 * width, (float) row / 8 * height, row);
+        drawHeart((float) col / 16 * width - 24, (float) row / 8 * height + 16, drawsOrNot(row, col));
+      }
+    }
+	
+  }
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  /**
+   * Given x and y coordinates, draws a stick figure in accordance to the colour input
+   *
+   * @param x  x-coordinates
+   * @param y  y-coordinates
+   *
+   */
+  private void drawGuy(float x, float y, int colour) 
+  {
+    // sets colours for the stick figure
+    noFill();
+    if (colour == 1) {
+      stroke (242, 24, 24);
+    }
+    else if (colour == 2) {
+      stroke (230, 142, 28);
+    }
+    else if (colour == 3) {
+      stroke (223, 230, 28);
+    }
+    else if (colour == 4) {
+      stroke (29, 153, 8);
+    }
+    else if (colour == 5) {
+      stroke (10, 120, 171);
+    }
+    else if (colour == 6) {
+      stroke (7, 53, 222);
+    }
+    else {
+      stroke (143, 58, 222);
+    }
+
+    // draws stick figure
+    ellipse(x, y - 30, width / 50, width / 50);
+    line (x, y - 30 + width / 100, x, y - 5);
+    line (x, y - 10, x + 10, y - 22);
+    line (x, y - 10, x - 10, y - 22);
+    line (x, y - 5, x + 10, y + 5);
+    line (x, y - 5, x - 10, y + 5);
+  }
+
+  /**
+   * Based on the number of row and coloum, determines whether an object should be drawn or not
+   *
+   * @param row  row number
+   * @param col  column number
+   * @return whether the object should or should not be drawn
+   *
+   */
+  private boolean drawsOrNot (int row, int col) {
+    // returns true if the location of the heart would match with the desired pattern
+    if (row % 2 == col % 2 && col != 1 && row != 7) {
+      return true;
+    }
+    return false;
   }
   
-  // define other methods down here.
+  /**
+   * Given x and y coordinates, draws a heart in accordance to the value of shouldDraw
+   *
+   * @param x  x-coordinates
+   * @param y  y-coordinates
+   * @param shouldDraw determines whether the heart should be drawn
+   *
+   */
+  private void drawHeart (float x, float y, boolean shouldDraw) {
+    // draws heart if shouldDraw indicates so
+    if (shouldDraw) {
+      stroke (0);
+      fill (0);
+      ellipse(x - 4, y - 12, 7, 7);
+      ellipse(x + 4, y - 12, 7, 7);
+      triangle(x - 7, y - 10, x + 7, y - 10, x, y - 3);
+    }
+  }
+
 }
